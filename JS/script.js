4864c1c46899c85ea650dafdde7be25ef95213fd
mitlinleonid <mitlinleonid@gmail.com>
2022-12-17 18:47:23 +0400

    немного изучил JS. переменные, типы, и тд

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,16 @@
-console.log(1);
+"use strict";
+
+// const result = confirm("ты тут?")
+// console.log(result)
+
+// const answer = +prompt("Сколько лет?", "1");
+// console.log(typeof(answer))
+
+// const answers = [];
+
+// answers[0] = prompt("name", "");
+// answers[1] = prompt("age", "");
+// answers[2] = prompt("JS", "");
+
+// console.log(typeof(answers));
+
